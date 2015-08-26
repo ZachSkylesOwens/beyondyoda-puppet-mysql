eb6a9681e4f088668289e8a3c7551df79935ef94
Zach Skyles Owens <zach@solcycle.net>
2015-08-26 00:22:57 -0400

    Delete db.pp

--- manifests/db.pp
+++
@@ -1,7 +0,0 @@
-define beyondyoda_mysql::db ( $user, $password ) {
-	exec { "create-${name}-db":
-		unless => "/usr/bin/mysql -u${user} -p${password} ${name}",
-		command => "/usr/bin/mysql -uroot -p$mysql_password -e \"CREATE DATABASE ${name}; GRANT ALL ON ${name}.* TO ${user}@localhost IDENTIFIED BY '$password';\"",
-		require => Service['mysql'],
-	}
-}
